814b4dea68b4ddc11ccaf2693d97c7807888eb14
jp25656 <toshiki.sasazaki@linecorp.com>
2024-12-17 15:09:37 +0900

    Enhance XSS application with body-parser, improved HTML structure, and refined input handling

--- xss/app.js
+++ xss/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { chromium } = require("playwright");
 
 const app = express();
@@ -8,56 +9,96 @@ const FLAG1 = process.env.FLAG_XSS1;
 const FLAG2 = process.env.FLAG_XSS2;
 const FLAG3 = process.env.FLAG_XSS3;
 
+const t = (s) => `<!DOCTYPE html>
+<html>
+<head>
+<title>XSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/holiday.css@0.11.2" />
+</head>
+<body>
+${s}
+</body>
+</html>
+`;
+
+app.use(bodyParser.urlencoded());
 // 問題1
 app.get("/xss1", (req, res) => {
   const username = req.query.username;
 
-  res.send(`
-        <h1>XSS Example</h1>
-        <p>Hello, ${username || "guest"}!</p>
-        <form method="GET" action="/xss1">
-            <input type="text" name="username">
-            <button type="submit">Submit</button>
-        </form>
-    `);
+  res.send(
+    t(`
+    <h1>XSS 1</h1>
+    <p>Hello, ${username || "guest"}!</p>
+    <form method="GET" action="/xss1">
+        <input type="text" name="username" placeholder="Enter your name">
+        <p><button type="submit">Submit</button></p>
+    </form>
+  `)
+  );
 });
 
 // 問題2
 app.get("/xss2", (req, res) => {
   let username = req.query.username;
-  username = username.replaceAll(/script|on/gi, "");
+  if (username) username = username.replace(/script|on/gi, "");
 
-  res.send(`
-        <h1>XSS Example 2</h1>
+  res.send(
+    t(`
+        <h1>XSS 2</h1>
         <p>Hello, ${username || "guest"}!</p>
         <form method="GET" action="/xss2">
-            <input type="text" name="username">
-            <button type="submit">Submit</button>
+            <input type="text" name="username" placeholder="Enter your name">
+            <p><button type="submit">Submit</button></p>
         </form>
-    `);
+    `)
+  );
 });
 
 // 問題3
 app.get("/xss3", (req, res) => {
   const username = req.query.username;
 
-  res.send(`
-            <h1>XSS Example 3</h1>
-            <p id=output></p>
-            <form method="GET" action="/xss3">
-                <input type="text" name="username">
-                <button type="submit">Submit</button>
-            </form>
-            <script>
-                const username = ${JSON.stringify(username || "guest")};
-                document.getElementById("output").innerText = "Hello, " + username + "!";
-            </script>
-        `);
+  res.send(
+    t(`
+        <h1>XSS 3</h1>
+        <p id=output></p>
+        <form method="GET" action="/xss3">
+            <input type="text" name="username" placeholder="Enter your name">
+            <p><button type="submit">Submit</button></p>
+        </form>
+        <script>
+            const username = ${JSON.stringify(username || "guest")};
+            document.getElementById("output").innerText = "Hello, " + username + "!";
+        </script>
+    `)
+  );
 });
 
 app.get("/validate", (req, res) => {
-  if (req.query.url) {
-    const url = new URL(req.query.url);
+  const url = (req.query.url || "")
+    .toString()
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+  res.send(
+    t(`
+        <h1>Validate XSS</h1>
+        <p>Submit a URL trigger an alert dialog</p>
+        <form method="POST" action="/validate">
+        <input type="text" name="url" value="${url}" placeholder="Enter URL">
+        <p><button type="submit">Submit</button></p>
+        </form>
+  `)
+  );
+});
+
+app.post("/validate", (req, res) => {
+  if (req.body.url) {
+    const url = new URL(req.body.url);
     url.host = "localhost:3000";
 
     (async () => {
@@ -66,15 +107,14 @@ app.get("/validate", (req, res) => {
       let is_alert = false;
 
       page.on("dialog", async (dialog) => {
+        await dialog.accept();
         if (dialog.type() === "alert") {
           console.log("Alert dialog detected");
-          await dialog.accept();
-          // Call your function here
           is_alert = true;
         }
       });
 
-      await page.goto(url.href, { timeout: 5000 });
+      await page.goto(url.href, { timeout: 3000 });
       await browser.close();
 
       if (is_alert) {
@@ -86,26 +126,36 @@ app.get("/validate", (req, res) => {
             : url.pathname == "/xss3"
             ? FLAG3
             : null;
-        res.send(`<h1>Attack</h1>
-        <p>Flag: ${flag}</p>`);
+        res.send(
+          t(`
+                <h1>XSS Succeeded on ${url.pathname}</h1>
+                <p>Flag: <code>${flag}</code></p>
+            `)
+        );
       } else {
-        res.send(`<h1>Safe</h1>
-        <p>URL is safe</p>`);
+        res.send(
+          t(`
+                <h1>Safe</h1>
+                <p>URL is safe</p>
+            `)
+        );
       }
     })().catch((error) => {
       console.error(error);
-      res.send(`<h1>Error</h1>
-        <p>Invalid URL</p>`);
+      res.send(
+        t(`
+            <h1>Error</h1>
+            <p>Invalid URL</p>
+          `)
+      );
     });
   } else {
-    res.send(`
-            <h1>Attack</h1>
-            <p>Try to steal the flag!</p>
-            <form method="GET" action="/validate">
-            <input type="text" name="url">
-            <button type="submit">Submit</button>
-            </form>
-            `);
+    res.send(
+      t(`
+        <h1>Error</h1>
+        <p>URL is required</p>
+        `)
+    );
   }
 });
 
